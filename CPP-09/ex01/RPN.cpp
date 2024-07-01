5f5c9fe2f8e561e28a94a6b0b5875bed8f336eba
aranger <aanger3@gmail.com>
2024-07-01 22:27:28 +0200

    ty

--- CPP-09/ex01/RPN.cpp
+++ CPP-09/ex01/RPN.cpp
@@ -6,13 +6,13 @@
 /*   By: aranger <aranger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 19:06:17 by aranger           #+#    #+#             */
-/*   Updated: 2024/07/01 13:28:05 by aranger          ###   ########.fr       */
+/*   Updated: 2024/07/01 22:25:13 by aranger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 
-static int		doOperation(int val1, int val2, char op)
+int		doOperation(int val1, int val2, char op)
 {
 	if (op == 57)
 	{
@@ -29,69 +29,22 @@ static int		doOperation(int val1, int val2, char op)
 	
 }
 
+std::string::iterator findValue(std::string str)
+{
+	
+}
+
 void	rpnCalculate(std::string entry)
 {
-	std::stack<int> 		stack;
-	size_t					start = 0;
-	std::string 			validNumbers = "0123456789";
-	std::string 			validOperator = "/*-+";
-	int						size = 0;
-	bool					space = true;
+	std::string::iterator	it;
 
-	for(size_t i = 0; i < entry.size(); i++)
-	{
-		if (std::isspace(entry[i]) == false && space == true)
-		{
-			start = i;
-			space = false;
-			size = 0;
-		}
-		if (validNumbers.find(entry[i]) == std::string::npos && validOperator.find(entry[i]) == std::string::npos && std::isspace(entry[i]) == false)
-		{
-			std::cerr << "Error : invalid input " << std::endl;
-			return;
-		}
-		if ((std::isspace(entry[i]) == true || i == entry.size()) && space == false)
-		{
-			if (validOperator.find(entry[i]) != std::string::npos)
-			{
-				if(stack.size() < 2)
-				{
-					std::cout << "stacksize < 2" << std::endl;
-					return;
-				}
-				else
-				{
-					int val1 = stack.top();
-					stack.pop();
-					int val2 = stack.top();
-					stack.pop();
-					stack.push(doOperation(val1, val2, entry[i]));
-					std::cout << "add" << std::endl;
-				}
-			}
-			else 
-			{
-				std::cout << "add value" << std::endl;
-				std::string tmp = entry.substr(start, i - start);
-				int value = std::atoi(tmp.c_str());
-				std::cout << value << std::endl;
-				if (value < 0 || value > 10)
-				{
-					std::cerr << "Error : invalid number" << std::endl;
-					return;
-				}
-				stack.push(value);
-			}
-			space = true;
-			size = 0;
-		}
-		if (size > 2)
-		{
-			std::cerr << "Error : invalid input size > 2" << std::endl;
-			return;
-		}
-		size++;
-	}
-	std::cout << "result = " << stack.top() << std::endl;
+	it = entry.begin();
+	std::cout << it.base() << std::endl;
+	it++;		
+	std::cout << it.base() << std::endl;
+	it++;
+	std::cout << it.base() << std::endl;
+	it++;
+	std::cout << it.base() << std::endl;
+	it++;	
 }
